d569559e0e589db4d9add7be00b72880ef7edf18
patrickmontalbo <patrickedward_montalbo@yahoo.com>
2018-05-03 16:39:32 +0800

    Updated Dashboard Interface

--- MPSystem/View/ucDashboard.Designer.cs
+++ MPSystem/View/ucDashboard.Designer.cs
@@ -29,12 +29,16 @@
         private void InitializeComponent()
         {
             this.dashGrid = new System.Windows.Forms.DataGridView();
-            this.btnUpdateLoad = new System.Windows.Forms.Button();
             this.checkOption = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.comPort = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.mobileNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.load = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Network = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSos = new System.Windows.Forms.Button();
+            this.btnCheckNum = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dashGrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,27 +57,10 @@
             this.mobileNumber,
             this.load,
             this.Network});
-            this.dashGrid.Location = new System.Drawing.Point(8, 61);
+            this.dashGrid.Location = new System.Drawing.Point(8, 67);
             this.dashGrid.Name = "dashGrid";
-            this.dashGrid.Size = new System.Drawing.Size(694, 462);
+            this.dashGrid.Size = new System.Drawing.Size(694, 424);
             this.dashGrid.TabIndex = 0;
-            // 
-            // btnUpdateLoad
-            // 
-            this.btnUpdateLoad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(204)))), ((int)(((byte)(144)))));
-            this.btnUpdateLoad.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnUpdateLoad.FlatAppearance.BorderSize = 0;
-            this.btnUpdateLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUpdateLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUpdateLoad.ForeColor = System.Drawing.Color.White;
-            this.btnUpdateLoad.Location = new System.Drawing.Point(8, 6);
-            this.btnUpdateLoad.Name = "btnUpdateLoad";
-            this.btnUpdateLoad.Size = new System.Drawing.Size(166, 33);
-            this.btnUpdateLoad.TabIndex = 1;
-            this.btnUpdateLoad.Text = "Update load balance";
-            this.btnUpdateLoad.UseVisualStyleBackColor = false;
-            this.btnUpdateLoad.Visible = false;
-            this.btnUpdateLoad.Click += new System.EventHandler(this.btnUpdateLoad_Click);
             // 
             // checkOption
             // 
@@ -105,11 +92,78 @@
             this.Network.ReadOnly = true;
             this.Network.Width = 150;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(10, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(103, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // btnSos
+            // 
+            this.btnSos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(204)))), ((int)(((byte)(144)))));
+            this.btnSos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSos.FlatAppearance.BorderSize = 0;
+            this.btnSos.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(214)))), ((int)(((byte)(151)))));
+            this.btnSos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(214)))), ((int)(((byte)(151)))));
+            this.btnSos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSos.ForeColor = System.Drawing.Color.White;
+            this.btnSos.Location = new System.Drawing.Point(8, 505);
+            this.btnSos.Margin = new System.Windows.Forms.Padding(0, 0, 3, 3);
+            this.btnSos.MaximumSize = new System.Drawing.Size(75, 21);
+            this.btnSos.MinimumSize = new System.Drawing.Size(75, 21);
+            this.btnSos.Name = "btnSos";
+            this.btnSos.Size = new System.Drawing.Size(75, 21);
+            this.btnSos.TabIndex = 22;
+            this.btnSos.Text = "SOS LOAD";
+            this.btnSos.UseVisualStyleBackColor = false;
+            // 
+            // btnCheckNum
+            // 
+            this.btnCheckNum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(204)))), ((int)(((byte)(144)))));
+            this.btnCheckNum.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCheckNum.FlatAppearance.BorderSize = 0;
+            this.btnCheckNum.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(214)))), ((int)(((byte)(151)))));
+            this.btnCheckNum.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(214)))), ((int)(((byte)(151)))));
+            this.btnCheckNum.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheckNum.ForeColor = System.Drawing.Color.White;
+            this.btnCheckNum.Location = new System.Drawing.Point(93, 505);
+            this.btnCheckNum.Margin = new System.Windows.Forms.Padding(0, 0, 3, 3);
+            this.btnCheckNum.MaximumSize = new System.Drawing.Size(95, 21);
+            this.btnCheckNum.MinimumSize = new System.Drawing.Size(95, 21);
+            this.btnCheckNum.Name = "btnCheckNum";
+            this.btnCheckNum.Size = new System.Drawing.Size(95, 21);
+            this.btnCheckNum.TabIndex = 23;
+            this.btnCheckNum.Text = "Check Numbers";
+            this.btnCheckNum.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Assign a dedicated Number/ Receiver";
+            // 
             // ucDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.btnUpdateLoad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnCheckNum);
+            this.Controls.Add(this.btnSos);
             this.Controls.Add(this.dashGrid);
             this.MaximumSize = new System.Drawing.Size(711, 536);
             this.MinimumSize = new System.Drawing.Size(711, 536);
@@ -118,17 +172,22 @@
             this.Load += new System.EventHandler(this.ucDashboard_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dashGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dashGrid;
-        public System.Windows.Forms.Button btnUpdateLoad;
         private System.Windows.Forms.DataGridViewCheckBoxColumn checkOption;
         private System.Windows.Forms.DataGridViewTextBoxColumn comPort;
         private System.Windows.Forms.DataGridViewTextBoxColumn mobileNumber;
         private System.Windows.Forms.DataGridViewTextBoxColumn load;
         private System.Windows.Forms.DataGridViewTextBoxColumn Network;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSos;
+        private System.Windows.Forms.Button btnCheckNum;
+        private System.Windows.Forms.Label label1;
     }
 }
